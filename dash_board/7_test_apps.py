554bbf23fc5dce7595e009de4d36f010512fd7a5
vimalv-AI <vsvimal5420@gmail.com>
2020-03-23 01:58:39 +0530

    Added m 21

--- dash_board/7_test_apps.py
+++ dash_board/7_test_apps.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('ph.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/vimalv-AI/Vimal/master/dash_board/ph.csv')
 
 fig = px.line(df, x='Date_x', y=',Potential Hydrogen_y', title='PH Values')
 fig.show()
